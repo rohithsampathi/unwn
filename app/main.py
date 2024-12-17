e005849e8d0b3ae6b5bf23b478024a6629ffcf27
Rohith Sampathi <skyg@Rohiths-MacBook-Pro.local>
2024-12-17 23:28:20 +0530

    firebase admin

--- app/main.py
+++ app/main.py
@@ -270,4 +270,6 @@ async def debug_last_analysis(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
